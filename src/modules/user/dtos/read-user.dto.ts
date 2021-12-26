5599294d86906509f238854dbc67c2c858c93293
Adrian Werle <adrianwerle@live.com.ar>
2021-12-25 21:56:51 -0300

    Refactorizacion: Dtos al estilo Nest.js cont.

--- src/modules/user/dtos/read-user.dto.ts
+++ src/modules/user/dtos/read-user.dto.ts
@@ -1,17 +1,27 @@
-import { Type } from 'class-transformer';
+import { Exclude, Expose, Type } from 'class-transformer';
 import { IsEmail, IsNumber, IsString } from 'class-validator';
 import { ReadUserDetailsDto } from './read-user-details.dto';
+import { ReadRoleDto } from '../../role/dtos/read-role.dto';
 
+@Exclude()
 export class ReadUserDto {
+    @Expose()
     @IsNumber()
     readonly id: number;
 
+    @Expose()
     @IsEmail()
     readonly email: string;
 
+    @Expose()
     @IsString()
     readonly username: string;
 
+    @Expose()
     @Type(() => ReadUserDetailsDto)
     readonly details: ReadUserDetailsDto;
+
+    @Expose()
+    @Type(() => ReadRoleDto)
+    readonly roles: ReadRoleDto[];
 }
